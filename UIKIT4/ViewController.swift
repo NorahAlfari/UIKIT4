7265fea8a8126c5c77da465df40c26d3c3faed66
NorahAlfari <nouraalfari@hotmail.com>
2023-12-17 10:03:06 +0300

    first commit

---
+++ UIKIT4/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UIKIT4
+//
+//  Created by Norah Alfari on 28/05/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+        
+    }
+
+   
+    
+}
+
